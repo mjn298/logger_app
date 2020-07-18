56374c9061686f71772a55f951b7d10916fb2692
mjn <mjn298@gmail.com>
2020-07-18 19:16:55 -0400

    stuff

--- build.sbt
+++ build.sbt
@@ -8,7 +8,8 @@ libraryDependencies ++= Seq(
   "org.typelevel" %% "cats-core" % "2.1.1",
   "org.typelevel" %% "cats-effect" % "2.1.4",
   "co.fs2" %% "fs2-core" % "2.4.2",
-  "co.fs2" %% "fs2-io" % "2.4.0"
+  "co.fs2" %% "fs2-io" % "2.4.0",
+  "info.fingo" %% "spata" % "0.8.0"
 )
 
 
--- src/main/scala/mjn/logger/LoggerApp.scala
+++ src/main/scala/mjn/logger/LoggerApp.scala
@@ -1,26 +1,33 @@
 package mjn.logger
 
-import mjn.logger.models.{LogGroupStats, LogLine, LoggerState}
+import java.nio.file.Path
 
-import scala.annotation.tailrec
+import cats.effect.{ExitCode, IO, IOApp}
+import fs2.Stream
+import info.fingo.spata.io.reader
 
-object LoggerApp {
-  def main(args: Array[String]): Unit = {
-    val bufferedSource = getClass.getResourceAsStream("/sample_csv.txt")
-    val lines = scala.io.Source.fromInputStream(bufferedSource).getLines.
-    process(lines.getLines., LoggerState())
+object LoggerApp extends IOApp {
+  def run(args: List[String]): IO[ExitCode] = {
+    IO[ExitCode](ExitCode.Success)
   }
 
-  def readLogLine(line: String): LogLine = {
-    val cols = line.split(",").map(_.trim)
-    LogLine(cols(0), cols(2), cols(3).toLong, cols(4), cols(5), cols(6).toInt)
-  }
-
-  @tailrec
-  def process(lines: LazyList[String], state: LoggerState): LogGroupStats = {
-    if(lines.isEmpty) state.stats
-    else {
-      process(lines.tail, state.update(readLogLine(lines.head)))
-    }
-  }
+  val stream: Stream[IO, Char] = reader.plain[IO].read(Path.of("sample_csv.txt"))
+  stream.compile.
+  //  def main(args: Array[String]): Unit = {
+  //    val bufferedSource = getClass.getResourceAsStream("/sample_csv.txt")
+  //    val lines = scala.io.Source.fromInputStream(bufferedSource)
+  //  }
+  //
+  //  def readLogLine(line: String): LogLine = {
+  //    val cols = line.split(",").map(_.trim)
+  //    LogLine(cols(0), cols(2), cols(3).toLong, cols(4), cols(5), cols(6).toInt)
+  //  }
+  //
+  //  @tailrec
+  //  def process(lines: LazyList[String], state: LoggerState): LogGroupStats = {
+  //    if(lines.isEmpty) state.stats
+  //    else {
+  //      process(lines.tail, state.update(readLogLine(lines.head)))
+  //    }
+  //  }
 }
